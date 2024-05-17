aa2add73780b5c2ab9b999b59bea28916681d3a3
Tarik Ozdemir <96834270+tarikozdemir@users.noreply.github.com>
2024-05-17 19:01:05 +0300

    ...

--- obj/Debug/net8.0/GalaxyWars.AssemblyInfo.cs
+++ obj/Debug/net8.0/GalaxyWars.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GalaxyWars")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca73abcaac46493a67e2d956995117dc376fe800")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86d09edbcf60119c70891ab4a880681f61434f6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GalaxyWars")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GalaxyWars")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
