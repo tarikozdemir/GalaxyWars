1f402d4d821e1af25dbbee473229ae6b59300ffb
Tarik Ozdemir <96834270+tarikozdemir@users.noreply.github.com>
2024-12-03 17:32:40 +0300

    .

--- Program.cs
+++ Program.cs
@@ -7,12 +7,7 @@ namespace GalaxyWars
     {
         static void Main(string[] args)
         {
-#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
-            Game game = null;
-#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
-
-            // Game nesnesini oluşturduktan sonra PlayerActionHandler ve FleetActionHandler oluşturulacak
-            game = new Game();
+            Game game = new Game();
             
             var playerActionHandler = new PlayerActionHandler(game);
             var fleetActionHandler = new FleetActionHandler(game);
